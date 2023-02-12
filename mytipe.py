8c214ee0fbbfd51e29faabbe4831306aff092c27
farad <002.bianco@gmail.com>
2023-02-12 14:20:59 +0100

    terza revisione

--- mytipe.py
+++ mytipe.py
@@ -7,23 +7,20 @@
 # - devo poter creare il mio set partendo da una lista, stringa o altri oggetti iterabili
 
 class s3t:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self):
+          self.value = []
 
+    def __add__(self, value):
+          self.value = [value]
     def __str__(self):
-        return f"[{self.value}]"
-
+        return f"{self.value}"
     def maremma(self):
         print("le stelle sono tante milioni di milione la stella di " + self.value + " vuol dire qualità")
         print(f"le stelle sono tante milioni di milione la stella di {self.value} vuol dire qualità")
+    def __getitem__():
+        pass
 
-    def __add__(self, value):
-        self.value = value
 
-
-p = []
-p.append(s3t("ciao"))
-
-#p.__add__("antonio")
-
-print(p)
+antonio = s3t()
+print(antonio.__str__())
+print(antonio[0].__add__("giovanni"))
